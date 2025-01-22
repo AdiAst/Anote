8a872ad35b09508e464b3e2422112704b738d890
Adi Astawa <adiast457@gmail.com>
2025-01-22 13:18:06 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
-import 'package:anote/widget/home.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:flutter/material.dart';
+// import 'package:anote/widget/home.dart';
+// import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+// import 'package:flutter/material.dart';
 
 void main() {
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-  runApp(const MyApp());
+  print("lorem");
+  // sqfliteFfiInit();
+  // databaseFactory = databaseFactoryFfi;
+  // runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ANOTE',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'ANOTE'),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'ANOTE',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(title: 'ANOTE'),
+//     );
+//   }
+// }
